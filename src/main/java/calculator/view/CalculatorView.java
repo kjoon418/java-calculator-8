8216ada2f47c9f1cde6fd917d89d131d06638c6d
Kim, Joon <kjoon418@naver.com>
2025-10-15 10:50:19 +0900

    feat(view): Console API를 이용해 입력을 처리하는 뷰 구현

---
+++ src/main/java/calculator/view/CalculatorView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorView {
+    public String readInput() {
+        return Console.readLine();
+    }
+}
