03db201bc91809cf55840775b68281fec9c1d851
Kim, Joon <kjoon418@naver.com>
2025-10-15 10:59:08 +0900

    chore(model): 입력 값이 존재하는지 검증하는 로직 추가

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import org.junit.platform.commons.util.StringUtils;
+
 import java.util.Arrays;
 
 public class StringCalculator {
@@ -10,10 +12,18 @@ public class StringCalculator {
     }
 
     public long sum(String input) {
+        validateInputNotEmpty(input);
+
         String regex = DelimiterRegexBuilder.build(defaultDelimiters);
 
         return Arrays.stream(input.split(regex))
                 .mapToLong(Long::parseLong)
                 .sum();
+    }
+
+    private void validateInputNotEmpty(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new IllegalArgumentException("입력 값이 존재하지 않거나 비어 있습니다.");
+        }
     }
 }
