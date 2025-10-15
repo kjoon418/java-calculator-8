e87627cc71b32dda6d33147f86f928f4680f3694
Kim, Joon <kjoon418@naver.com>
2025-10-15 10:55:15 +0900

    feat(application): `main` 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.model.StringCalculator;
+import calculator.view.CalculatorView;
+
 public class Application {
+    private static final String[] defaultDelimiters = {",", ":"};
+    private static final StringCalculator calculator = new StringCalculator(defaultDelimiters);
+    private static final CalculatorView view = new CalculatorView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = view.readInput();
+
+        long result = calculator.sum(input);
+
+        view.printResult(result);
     }
 }
